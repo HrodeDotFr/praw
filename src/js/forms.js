5add751c6946bd6ae098da3eb2a0812182eba9d3
Lucas <lucas@lucas-PC-portable.home>
2021-02-18 18:56:58 +0100

    Ajout $.preparePost

---
+++ src/js/forms.js
@@ -0,0 +1,17 @@
+jQuery.fn.preparePost = function (additionalData) {
+    var data = {};
+    var sel = this;
+    if (sel.is("input") || sel.is("textarea") || sel.is("select")) {
+        sel = $(this).append(sel);
+    }
+
+    $.each(sel.find('input,select,textarea').filter(':enabled').serializeArray(), function (i, v) {
+        data[v.name] = v.value;
+    });
+
+    if (additionalData) {
+        data = $.extend(data, additionalData);
+    }
+
+    return data;
+};
