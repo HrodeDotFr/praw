50c53f8c79145a8669b92ea6570033a842cdddd5
Lucas <lucas@lucas-PC-portable.home>
2021-02-18 19:54:46 +0100

    Ajout de $.disable et $.enable

--- src/js/forms.js
+++ src/js/forms.js
@@ -5,7 +5,7 @@ jQuery.fn.preparePost = function (additionalData) {
         sel = $('<div>').append(sel);
     }
 
-    $.each(sel.find('input,select,textarea').filter(':enabled').serializeArray(), function (i, v) {
+    $.each(sel.find("input,select,textarea").filter(':enabled').serializeArray(), function (i, v) {
         data[v.name] = v.value;
     });
 
@@ -14,4 +14,20 @@ jQuery.fn.preparePost = function (additionalData) {
     }
 
     return data;
+};
+
+jQuery.fn.disable = function (additionalData) {
+    var sel = this;
+    if (sel.is("input") || sel.is("textarea") || sel.is("select")) {
+        sel = $('<div>').append(sel);
+    }
+    sel.find("input,select,textarea,button").filter(':enabled').prop("disabled", true);
+};
+
+jQuery.fn.enable = function (additionalData) {
+    var sel = this;
+    if (sel.is("input") || sel.is("textarea") || sel.is("select")) {
+        sel = $('<div>').append(sel);
+    }
+    sel.find("input,select,textarea,button").filter(':disabled').prop("disabled", false);
 };
